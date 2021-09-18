dddd3e8ae9eb19d9a8ce09663e77b0456eb393a3
lstebe <84880723+lstebe@users.noreply.github.com>
2021-09-18 14:57:46 +0200

    Update fetchNR.sh

--- fetchNR.sh
+++ fetchNR.sh
@@ -4,7 +4,8 @@
 #bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 echo "Fetch Installation Script"
 curl -o installnr.sh https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered
-bash ./installnr.sh
+bash ./installnr.sh --confirm-install --confirm-pi
 echo "Installing NPM and"
 echo "Installing NODE RED and setup as Service"
 exit 0
+curl -o installnr.sh https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered
